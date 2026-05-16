7de28fd5655198a7e256a58d2debc9a598f6c9ae
Leonardo <lleocastro40@gmail.com>
2026-05-16 18:14:55 -0300

    issue 113 por Leonardo Sobrinho: alteraçoes para aparecer a tag em menu suspenso

--- src/eucs_platform/context_processors.py
+++ src/eucs_platform/context_processors.py
@@ -1,9 +1,11 @@
 from django.conf import settings
-
+from organisations.models import OrganisationType
 
 def global_settings(request):
+    org_types = OrganisationType.objects.values('type').distinct().order_by('type')
     return {
         'TRANSLATED_LANGUAGES': settings.TRANSLATED_LANGUAGES,
         'FEATURED_MGT': settings.FEATURED_MGT,
-        'FORUM_ENABLED': settings.FORUM_ENABLED
+        'FORUM_ENABLED': settings.FORUM_ENABLED,
+        'all_organisation_types': org_types
     }
