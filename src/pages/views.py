1b7ab7a863ad1e05188087585cb19b9b19c1e265
Leonardo <lleocastro40@gmail.com>
2026-06-27 09:17:59 -0300

    issue 145 por Leonardo Sobrinho: ajustando layout da pagina de depoimentos, valorizando o texto e paginado

--- src/pages/views.py
+++ src/pages/views.py
@@ -1,7 +1,8 @@
 from django.utils.translation import get_language
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
+from django.core.paginator import Paginator
 
-from .models import Page
+from .models import Page, Section
 
 
 class PageDetailView(DetailView):
@@ -15,3 +16,24 @@ class PageDetailView(DetailView):
     def get_queryset(self):
         current_language = get_language()
         return Page.objects.filter(slug=self.kwargs['slug'], language=current_language)
+
+
+class DepoimentosListView(ListView):
+    model = Section
+    template_name = 'pages/depoimentos.html'
+    context_object_name = 'page_obj'
+    paginate_by = 6 
+
+    def get_queryset(self):
+        return Section.objects.filter(page__slug='depoimentos').order_by('order')
+        
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        current_language = get_language()
+        
+        try:
+            context['page'] = Page.objects.get(slug='depoimentos', language=current_language)
+        except Page.DoesNotExist:
+            context['page'] = None
+            
+        return context
