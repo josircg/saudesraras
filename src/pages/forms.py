16407b70ac495df671bfbf3b903b18671eb29b48
Leonardo <lleocastro40@gmail.com>
2026-07-23 22:51:14 -0300

    issue 77 por Leonardo Sobrinho: ajuste nos formularios

--- src/pages/forms.py
+++ src/pages/forms.py
@@ -1,17 +1,28 @@
 from django import forms
+from ckeditor.widgets import CKEditorWidget
 from .models import Section, Page, Article
 
 class SectionForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorWidget(config_name='admin'), required=False)
+    header = forms.CharField(widget=CKEditorWidget(config_name='admin'), required=False)
+
     class Meta:
         model = Section
-        fields = [ 'title', 'order', 'header', 'content', 'visible']
+        fields = ['title', 'order', 'header', 'content', 'visible']
+
 
 class PageForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorWidget(config_name='admin'), required=False)
+
     class Meta:
         model = Page
-        fields = ['title','content', 'image']
+        fields = ['title', 'content', 'image']
+
 
 class ArticleForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorWidget(config_name='admin'), required=False)
+    header = forms.CharField(widget=CKEditorWidget(config_name='admin'), required=False)
+
     class Meta:
         model = Article
         fields = ['title', 'order', 'header', 'content']
